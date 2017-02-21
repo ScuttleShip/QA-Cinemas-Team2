49d0d5a5e6bd8f09742fc973b020bb365b8958f1
John <jgwhittaker93@gmail.com>
2017-02-21 17:05:48 +0000

    Booking Entity and Test

---
+++ src/test/java/com/qa/cinema/persistence/BookingTest.java
@@ -0,0 +1,42 @@
+package com.qa.cinema.persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookingTest {
+	
+	@Test
+	public void testBookingIDSetter() {
+		Booking testBook = new Booking(3, "test@test.com");
+		testBook.setBookingID((long) 2);
+		assertEquals(2, testBook.getBookingID().longValue());
+	}
+	
+	@Test
+	public void numberOfSeatsGetter() {
+		Booking testBook = new Booking(3, "test@test.com");
+		assertEquals(3, testBook.getNumberOfSeats());
+	}
+	
+	@Test
+	public void numberOfSeatsIDSetter() {
+		Booking testBook = new Booking(3, "test@test.com");
+		testBook.setNumberOfSeats(2);
+		assertEquals(2, testBook.getNumberOfSeats());
+	}
+	
+	@Test
+	public void customerEmailGetter() {
+		Booking testBook = new Booking(3, "test@test.com");
+		assertEquals("test@test.com", testBook.getCustomerEmail());
+	}
+	
+	@Test
+	public void customerEmailSetter() {
+		Booking testBook = new Booking(3, "test@test.com");
+		testBook.setCustomerEmail("TEST@TEST.com");
+		assertEquals("TEST@TEST.com",testBook.getCustomerEmail());
+	}
+
+}
