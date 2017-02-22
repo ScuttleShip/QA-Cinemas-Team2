7ab9b0b7712c107a43c633b3b4eedaff1f46549a
ScuttleShip <rupert_langford@hotmail.com>
2017-02-22 14:04:12 +0000

    added endpoint

---
+++ src/main/java/com/qa/cinema/rest/VenueEndPoint.java
@@ -0,0 +1,48 @@
+package com.qa.cinema.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.qa.cinema.service.VenueServiceDBImpl;
+
+@Path("/cinema")
+public class VenueEndPoint {
+
+	@Inject
+	private VenueServiceDBImpl venueService;
+	
+	@GET
+	@Path("/json")
+	@Produces({ "application/json" })
+	public String getVenueAsJson(){
+		return venueService.getAllVenues();
+		
+	}
+	
+	@POST
+	@Path("/json")
+	@Produces({ "application/json" })
+	public String addNewVenueToMap(String venueJson){
+		return venueService.addNewVenue(venueJson);
+	}
+	
+	@PUT
+	@Path("/json/{id}")
+	@Produces({ "application/json" })
+	public String updateVenueFromCinema(@PathParam("id")Long id, String venueJson){
+		return venueService.updateVenue(id, venueJson);
+	}
+	
+	@DELETE
+	@Path("/json/{id}")
+	@Produces({ "application/json" })
+	public String deleteVenueFromCinema(@PathParam("id")Long id){
+		return venueService.deleteVenue(id);
+	}
+}
