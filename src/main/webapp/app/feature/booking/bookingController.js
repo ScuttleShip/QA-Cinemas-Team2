a97facacc82298a7a32027768d4e82e0e3d60dd5
lameay <leimeih@gmail.com>
2017-03-02 14:08:32 +0000

    Some HTML for checkout page added!

--- src/main/webapp/app/feature/booking/bookingController.js
+++ src/main/webapp/app/feature/booking/bookingController.js
@@ -3,10 +3,48 @@
  */
 (function() {
 
-    var bookingController = function() {
+    var bookingController = function(bookingService) {
+
+        var vm = this;
+
+        function init() {
+
+            sessionStorage.setItem("chosenVenue", "Venue1");
+            sessionStorage.setItem("chosenDate", "2017-09-21");
+            sessionStorage.setItem("chosenShowing", "1");
+            sessionStorage.setItem("chosenNumberOfSeats", "3");
+
+            var chosenVenue = sessionStorage.getItem("chosenVenue");
+            var chosenDate = sessionStorage.getItem("chosenDate");
+            var chosenShowing = sessionStorage.getItem("chosenShowing");
+            var chosenNumberOfSeats = sessionStorage.getItem("chosenNumberOfSeats");
+
+            var venuePlace = document.getElementById("chosenVenue");
+            var datePlace = document.getElementById("chosenDate");
+            var showingPlace = document.getElementById("chosenShowing");
+            var seatsPlace = document.getElementById("chosenNumberOfSeats");
+
+            venuePlace.innerHTML = chosenVenue;
+            datePlace.innerHTML = chosenDate;
+            showingPlace.innerHTML = chosenShowing;
+            seatsPlace.innerHTML = chosenNumberOfSeats;
+
+        }
+
+        init();
+
+        vm.saveBooking = function() {
+
+            var booking = {};
+            booking.numberOfSeats = sessionStorage.getItem("chosenNumberOfSeats");
+            booking.customerEmail =
+
+            bookingService.saveBooking();
+
+        }
 
     };
 
-    angular.module("qaCinemas2").controller("bookingController", [bookingController]);
+    angular.module("qaCinemas2").controller("bookingController", ['bookingService', bookingController]);
 
 }());
