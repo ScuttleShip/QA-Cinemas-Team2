879fe12033583b6a401a4bf48d3333023976125e
John <jgwhittaker93@gmail.com>
2017-03-02 09:30:16 +0000

    JOHN W UX BRANCH

--- src/main/webapp/app/feature/home/homeController.js
+++ src/main/webapp/app/feature/home/homeController.js
@@ -1,3 +1,46 @@
 /**
  * Created by Administrator on 27/02/2017.
  */
+(function() {
+
+    var homeController = function(venueService) {
+
+        var vm = this;
+
+        vm.venues = [];
+
+        vm.saveVenueDate = function() {
+
+            var venueChooser = document.getElementById("myVenue");
+
+            var dateChooser = document.getElementById("selectedDate");
+
+            sessionStorage.setItem("chosenVenue", venueChooser.value);
+            sessionStorage.setItem("chosenDate", dateChooser.value);
+
+            console.log(sessionStorage.getItem("chosenVenue"));
+            console.log(sessionStorage.getItem("chosenDate"));
+
+
+
+        }
+
+        function init() {
+            venueService.getAllVenues().then(function (results) {
+                console.log(results);
+                vm.venues = results;
+            }, function (error) {
+                console.log("This is not working" + error);
+                vm.error=true;
+                vm.errorMessage = error;
+
+            });
+        }
+
+        init();
+
+    }
+
+    angular.module("qaCinemas2").controller("homeController", ['venueService', homeController]);
+
+}());
--- src/main/webapp/app/feature/venues/venuesController.js
+++ src/main/webapp/app/feature/venues/venuesController.js
@@ -1,3 +1,21 @@
-/**
- * Created by Administrator on 27/02/2017.
- */
+(function(){
+    var venueController = function(venueService)
+    {
+        var vm = this;
+
+        function init() {
+            venueService.getAllVenues().then(function (results) {
+                console.log(results);
+                vm.venues = results;
+            }, function (error) {
+                console.log("This is not working" + error);
+                vm.error=true;
+                vm.errorMessage = error;
+
+            });
+            }
+
+            init()
+    };
+    angular.module('qaCinemas2').controller('venueController', ['venueService', venueController]);
+}());
