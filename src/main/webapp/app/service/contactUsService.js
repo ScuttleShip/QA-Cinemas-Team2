f760a3572ffba7107e31464b21eddc675db33b38
trevellpaul <trevellpaul@gmail.com>
2017-03-03 11:13:05 +0000

    Service for Contact Us page created

---
+++ src/main/webapp/app/service/contactUsService.js
@@ -0,0 +1,13 @@
+(function() {
+
+    var contactUsService = function(contactUsDal) {
+
+        this.sendEmail = function(emailToSend) {
+            return contactUsDal.sendEmail(emailToSend);
+        }
+
+    }
+
+    angular.module("qaCinemas2").service("contactUsService", ["contactUsDal", contactUsService()]);
+
+}());
