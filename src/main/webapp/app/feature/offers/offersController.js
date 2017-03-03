db2106f9405e8a5b3d1601860b647ce0a98d804e
Amit Samuel <amitsamuel@hotmail.com>
2017-03-03 14:03:28 +0000

    Made sure the offers page is connected

--- src/main/webapp/app/feature/offers/offersController.js
+++ src/main/webapp/app/feature/offers/offersController.js
@@ -1,3 +1,12 @@
 /**
  * Created by Administrator on 27/02/2017.
  */
+(function() {
+
+    var offersController = function() {
+
+    };
+
+    angular.module("qaCinemas2").controller("offersController", [offersController]);
+
+}());
