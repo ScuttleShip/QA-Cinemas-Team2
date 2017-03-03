6d4f7a8ccd7d0522fc1338394c324200717307c3
trevellpaul <trevellpaul@gmail.com>
2017-03-03 11:14:51 +0000

    Controller for Contact Us page created

--- src/main/webapp/app/feature/contact/contactController.js
+++ src/main/webapp/app/feature/contact/contactController.js
@@ -1,3 +1,37 @@
-/**
- * Created by Administrator on 27/02/2017.
- */
+//You should create the validator only after the definition of the HTML form
+
+(function() {
+
+    var contactController = function(contactUsService) {
+
+        var frmvalidator  = new Validator("myform");
+        frmvalidator.addValidation("FirstName","req","Please enter your First Name");
+        frmvalidator.addValidation("FirstName","maxlen=20",	"Max length for FirstName is 20");
+        frmvalidator.addValidation("FirstName","alpha","Alphabetic chars only");
+
+        frmvalidator.addValidation("LastName","req","Please enter your Last Name");
+        frmvalidator.addValidation("LastName","maxlen=20","Max length is 20");
+
+        frmvalidator.addValidation("Email","maxlen=50");
+        frmvalidator.addValidation("Email","req");
+        frmvalidator.addValidation("Email","email");
+
+        frmvalidator.addValidation("Phone","maxlen=11");
+        frmvalidator.addValidation("Phone","numeric");
+
+        var venues = [];
+
+        function init() {
+
+        }
+
+        vm.sendEmail = function(emailToSend) {
+            var emailToJson = JSON.stringify(emailToSend);
+            contactUsService.sendEmail(emailToJson);
+        }
+
+    }
+
+    angular.module("qaCinemas2").controller("contactController", ["contactUsService", contactController]);
+
+}());
