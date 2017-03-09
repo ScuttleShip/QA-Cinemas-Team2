8d02f613c63d83646a613153d7cfc5f77146c39d
John <jgwhittaker93@gmail.com>
2017-03-09 15:04:10 +0000

    Final UX

--- src/main/webapp/app/feature/faqs/faqsController.js
+++ src/main/webapp/app/feature/faqs/faqsController.js
@@ -1,17 +1,31 @@
-(function()
-{
-    var acc = document.getElementsByClassName("accordion");
+(function() {
 
-    for (var i = 0; i < acc.length; i++)
-    {
-        acc[i].onclick = function()
-        {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block")
-                panel.style.display = "none";
-            else
-                panel.style.display = "block";
+    var faqsController = function() {
+
+        function init() {
+
+            var acc = document.getElementsByClassName("accordion");
+
+            for (var i = 0; i < acc.length; i++) {
+
+                acc[i].onclick = function() {
+                    this.classList.toggle("active");
+
+                    var panel = this.nextElementSibling;
+                    if (panel.style.display === "block") {
+                        panel.style.display = "none";
+                    }
+                    else {
+                        panel.style.display = "block";
+                    }
+                }
+
+            }
         }
+
+        init();
     }
+
+    angular.module("qaCinemas2").controller('faqsController', [faqsController]);
+
 }());
